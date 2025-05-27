e7e09d34b60436fb7cb923b24fd3f74fbe63c2c0
priya <priyankbhalala1911@gmail.com>
2025-05-27 18:02:47 +0530

    Update CORS configuration to use a hardcoded frontend URL

--- config/socket.ts
+++ config/socket.ts
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "https://chat-app-smoky-phi.vercel.app/",
     credentials: true,
   },
 });
