6f99ed3079bde3b50a077f1207181b3310304b1b
priya <priyankbhalala1911@gmail.com>
2025-05-27 17:20:37 +0530

    Refactor database configuration: update entity paths and migration file extensions

--- config/database.ts
+++ config/database.ts
@@ -1,27 +1,28 @@
+import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "../models/user";
 import { Message } from "../models/message";
 import { Conversation } from "../models/conversation";
 import "dotenv/config";
 import { initialSocketServer } from "./socket";
-import "reflect-metadata";
 
 export const AppDataSourse = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
-  entities: [User, Message, Conversation],
+  entities: ["dist/models/*.js"],
   ssl: {
     rejectUnauthorized: true,
   },
-  migrations: ["migrations/*.ts"],
+  migrations: ["migrations/*.js"],
 });
 
 export const intializeDatabase = async () => {
   try {
-    await AppDataSourse.initialize();
-    initialSocketServer();
+    console.log(process.env.DATABASE_URL);
+    // await AppDataSourse.initialize();
+    // initialSocketServer();
     console.log("Database connection established successfully.");
   } catch (error) {
     console.log("Error connecting to the database", error);
